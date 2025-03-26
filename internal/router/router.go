8ff5d355cfa7edcbcb3fa156cbc83d1625f08f22
Hans Brinck <12492806+ninlil@users.noreply.github.com>
2025-03-26 08:18:07 +0100

    add cleanup-handler with http-test

--- internal/router/router.go
+++ internal/router/router.go
@@ -54,6 +54,7 @@ func New(listenAddr, requestName, authKey, backend string, auth types.AuthProvid
 
 	proxy.mux = chi.NewRouter()
 	proxy.mux.Use(
+		proxy.CleanupHandler,
 		proxy.WrapHandler,
 		metrics.Wrap,
 		proxy.authHandler,
@@ -96,6 +97,25 @@ func (proxy *Proxy) Close() {
 		proxy.server.Shutdown(ctx)
 		defer cancel()
 	}
+}
+
+// CleanupHandler removes incoming headers that shouldn't be there
+// (like any X-Restrego header which is considered spoofing-attempts)
+func (proxy *Proxy) CleanupHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var toClean []string
+		for key := range r.Header {
+			if strings.HasPrefix(key, "X-Restrego-") {
+				toClean = append(toClean, key)
+			}
+		}
+		for _, key := range toClean {
+			slog.Warn("removing header (possible spoofing)", "header", key)
+			r.Header.Del(key)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (proxy *Proxy) authHandler(next http.Handler) http.Handler {
